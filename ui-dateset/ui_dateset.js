f2ba724da1291807c560dacd46ee3e38378de983
成岡 <naruoka-m@sgnet.co.jp>
2022-01-06 20:08:15 +0900

    サイズ変更

--- ui-dateset/ui_dateset.js
+++ ui-dateset/ui_dateset.js
@@ -11,16 +11,14 @@ module.exports = function(RED) {
     function HTML() {
         // define HTML code
         var html = String.raw`
-        <div style="width:250px; text-align:left">
-            <input type="datetime-local" step="1" id="sdatetime" style="width:180px; height:25px; font-size: 70%;"
+        <div style="width:350px; text-align:center">
+            <input type="datetime-local" step="1" id="sdatetime" style="width:160px; height:10px; font-size: 60%;"
                 ng-model="sdatetime">
-        </div>
-        <div align="center" style="width:250px; text-align:right";>
-            ～　<input type="datetime-local" step="1" id="edatetime" style="width:180px; height:25px; font-size: 70%;"
+             ~ <input type="datetime-local" step="1" id="edatetime" style="width:160px; height:10px; font-size: 60%;"
                 ng-model="edatetime">
         </div>
-        <div style="margin-top:5px; width:250px; text-align:center">
-            <input type="button" id="send" style="width:240px; height:30px; font-size: 70%;" value="更新" ng-click="click(sdatetime ,edatetime)">
+        <div style="width:350px; text-align:center">
+            <input type="button" id="send" style="width:300px; height:18px; font-size: 60%;" value="更新" ng-click="click(sdatetime ,edatetime)">
         </div>
         `;
 
